86768942a2c735875b9d039225d45659e85bf16a
Kjetil F <kjefriborg@gmail.com>
2023-07-11 20:51:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def getHardTotal(playerHand):
     hardTotal = 0
     for card in playerHand:
@@ -37,133 +40,136 @@ def hardHand(playerHand, dealerCard):
         if getHardTotal(playerHand) <= 9:
             return "Hit!"
         #Player card is 10 or 11
-        elif 10 <= getHardTotal (playerHand) <= 11:
+        elif 10 <= getHardTotal(playerHand) <= 11:
             return "Double!"
         #Player card is 12
-        elif 10 <= getHardTotal (playerHand) == 12:
+        elif 10 <= getHardTotal(playerHand) == 12:
             return "Hit!"
         #Player card is 13 or above
-        elif 12 <= getHardTotal (playerHand) >= 13:
+        elif 12 <= getHardTotal(playerHand) >= 13:
             return "Stand!"
         else:
             return "Unknown"
     # Dealer showing a 3
-    elif dealerCard == 3
+    elif dealerCard == 3:
         #Player card is 8 or below
         if getHardTotal(playerHand) <= 8:
             return "Hit!"
         #Player card is 9, 10 or 11
-        elif 9 <= getHardTotal (playerHand) <= 11:
+        elif 9 <= getHardTotal(playerHand) <= 11:
             return "Double!"
         #Player card is 12
-        elif 10 <= getHardTotal (playerHand) == 12:
+        elif 10 <= getHardTotal(playerHand) == 12:
             return "Hit!"
         #Player card is 13 or above
-        elif 12 <= getHardTotal (playerHand) >= 13:
+        elif 12 <= getHardTotal(playerHand) >= 13:
             return "Stand!"
     # Dealer showing a 4
-    elif dealerCard == 4
+    elif dealerCard == 4:
         #Player card is 8 or below
         if getHardTotal(playerHand) <= 8:
             return "Hit!"
         #Player card is 9, 10 or 11
-        elif 9 <= getHardTotal (playerHand) <= 11:
+        elif 9 <= getHardTotal(playerHand) <= 11:
             return "Double!"
         #Player card is 12 or above
-        elif 12 <= getHardTotal (playerHand) >= 12:
+        elif 12 <= getHardTotal(playerHand) >= 12:
             return "Stand!"
     # Dealer showing a 5
-    elif dealerCard == 5
+    elif dealerCard == 5:
         #Player card is 8 or below
         if getHardTotal(playerHand) <= 8:
             return "Hit!"
         #Player card is 9, 10 or 11
-        elif 9 <= getHardTotal (playerHand) <= 11:
+        elif 9 <= getHardTotal(playerHand) <= 11:
             return "Double!"
         #Player card is 12 or above
-        elif 12 <= getHardTotal (playerHand) >= 12:
+        elif 12 <= getHardTotal(playerHand) >= 12:
             return "Stand!"
     # Dealer showing a 6
-    elif dealerCard == 6
+    elif dealerCard == 6:
         #Player card is 8 or below
         if getHardTotal(playerHand) <= 8:
             return "Hit!"
         #Player card is 9, 10 or 11
-        elif 9 <= getHardTotal (playerHand) <= 11:
+        elif 9 <= getHardTotal(playerHand) <= 11:
             return "Double!"
         #Player card is 12 or above
-        elif 12 <= getHardTotal (playerHand) >= 12:
+        elif 12 <= getHardTotal(playerHand) >= 12:
             return "Stand!"
     # Dealer showing a 7
-    elif dealerCard == 7
+    elif dealerCard == 7:
         #Player card is 9 or below
         if getHardTotal(playerHand) <= 9:
             return "Hit!"
         #Player card is 9, 10 or 11
-        elif 10 <= getHardTotal (playerHand) <= 11:
+        elif 10 <= getHardTotal(playerHand) <= 11:
             return "Double!"
         #Player card is 12 to 16
-        elif 12 <= getHardTotal (playerHand) <= 16:
+        elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
         #Player card is 17
-        elif getHardTotal (playerHand) >= 17:
+        elif getHardTotal(playerHand) >= 17:
             return "Stand!"
     # Dealer showing a 8
-    elif dealerCard == 8
+    elif dealerCard == 8:
         #Player card is 9 or below
         if getHardTotal(playerHand) <= 9:
             return "Hit!"
         #Player card is 9, 10 or 11
-        elif 10 <= getHardTotal (playerHand) <= 11:
+        elif 10 <= getHardTotal(playerHand) <= 11:
             return "Double!"
         #Player card is 12 to 16
-        elif 12 <= getHardTotal (playerHand) <= 16:
+        elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
         #Player card is 17
-        elif getHardTotal (playerHand) >= 17:
+        elif getHardTotal(playerHand) >= 17:
             return "Stand!"
     # Dealer showing a 9
-    elif dealerCard == 9
+    elif dealerCard == 9:
         #Player card is 9 or below
         if getHardTotal(playerHand) <= 9:
             return "Hit!"
         #Player card is 9, 10 or 11
-        elif 10 <= getHardTotal (playerHand) <= 11:
+        elif 10 <= getHardTotal(playerHand) <= 11:
             return "Double!"
         #Player card is 12 to 16
-        elif 12 <= getHardTotal (playerHand) <= 16:
+        elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
         #Player card is 17
-        elif getHardTotal (playerHand) >= 17:
+        elif getHardTotal(playerHand) >= 17:
             return "Stand!"
     # Dealer showing a 10
-    elif dealerCard == 10
+    elif dealerCard == 10:
         #Player card is 10 or below
         if getHardTotal(playerHand) <= 10:
             return "Hit!"
         #Player card is 9, 10 or 11
-        elif getHardTotal (playerHand) == 11:
+        elif getHardTotal(playerHand) == 11:
             return "Double!"
         #Player card is 12 to 16
-        elif 12 <= getHardTotal (playerHand) <= 16:
+        elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
         #Player card is 17
-        elif getHardTotal (playerHand) >= 17:
+        elif getHardTotal(playerHand) >= 17:
             return "Stand!"
     # Dealer showing an Ace
-    elif dealerCard == 11
+    elif dealerCard == 11:
         #Player card is 10 or below
         if getHardTotal(playerHand) <= 10:
             return "Hit!"
         #Player card is 9, 10 or 11
-        elif getHardTotal (playerHand) == 11:
+        elif getHardTotal(playerHand) == 11:
             return "Double!"
         #Player card is 12 to 16
-        elif 12 <= getHardTotal (playerHand) <= 16:
+        elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
-        #Player card is 17
-        elif getHardTotal (playerHand) >= 17:
+        #Player card is between 17 and 20
+        elif 17 <= getHardTotal(playerHand) <= 20:
             return "Stand!"
+        #Player card is 21
+        elif getHardTotal(playerHand) == 21:
+            return "Blackjack!"
     else:
         return "Unknown dealer card!"
     
@@ -172,12 +178,12 @@ def hardHand(playerHand, dealerCard):
 
 # print(handIsSoft([11, 9]))
 
-hand = [7, 1, 11, 11, 1]
+hand = [random.randint(2,11), random.randint(2,11)]
+dealerCard = random.randint(2,11)
 
-if handIsSoft(hand):
-    print(getSoftTotal(hand))
-else:
-    print(getHardTotal(hand))
+
+print(str(getHardTotal(hand)) + " " + str(dealerCard))
+print(hardHand(hand, dealerCard))
 
 
 
