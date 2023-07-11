f283cda7c28656f3628a634ce731917970e00322
Kjetil F <kjefriborg@gmail.com>
2023-07-11 21:07:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def hardHand(playerHand, dealerCard):
         #Player card is 12 to 16
         elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
-        #Player card is 17
+        #Player card is 17 or above
         elif getHardTotal(playerHand) >= 17:
             return "Stand!"
     # Dealer showing a 8
@@ -122,7 +122,7 @@ def hardHand(playerHand, dealerCard):
         #Player card is 12 to 16
         elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
-        #Player card is 17
+        #Player card is 17 or above
         elif getHardTotal(playerHand) >= 17:
             return "Stand!"
     # Dealer showing a 9
@@ -136,7 +136,7 @@ def hardHand(playerHand, dealerCard):
         #Player card is 12 to 16
         elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
-        #Player card is 17
+        #Player card is 17 or above
         elif getHardTotal(playerHand) >= 17:
             return "Stand!"
     # Dealer showing a 10
@@ -150,7 +150,7 @@ def hardHand(playerHand, dealerCard):
         #Player card is 12 to 16
         elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
-        #Player card is 17
+        #Player card is 17 or above
         elif getHardTotal(playerHand) >= 17:
             return "Stand!"
     # Dealer showing an Ace
@@ -164,17 +164,120 @@ def hardHand(playerHand, dealerCard):
         #Player card is 12 to 16
         elif 12 <= getHardTotal(playerHand) <= 16:
             return "Hit!"
-        #Player card is between 17 and 20
-        elif 17 <= getHardTotal(playerHand) <= 20:
+        #Player card is 17 or above
+        elif getHardTotal(playerHand) >= 17:
             return "Stand!"
-        #Player card is 21
-        elif getHardTotal(playerHand) == 21:
-            return "Blackjack!"
     else:
         return "Unknown dealer card!"
     
     
-
+def softHand(playerHand, dealerCard):
+    # Dealer showing a 2
+    if dealerCard == 2:
+        #Player card is Ace + 2 to Ace + 6
+        if getSoftTotal(playerHand) <= 17:
+            return "Hit!"
+        #Player card is Ace + 7
+        elif getSoftTotal(playerHand) == 18:
+            return "Double! or Stand!"
+        #Player card is Ace + 8 or above
+        elif getSoftTotal(playerHand) >= 19:
+            return "Stand"
+    # Dealer showing a 3
+    elif dealerCard == 3:
+        #Player card is Ace + 2 to Ace + 5
+        if getSoftTotal(playerHand) <= 16:
+            return "Hit!"
+        #Player card is Ace + 6
+        elif getSoftTotal(playerHand) == 17:
+            return "Double!"
+        #Player card is Ace + 7
+        elif getSoftTotal(playerHand) == 18:
+            return "Double! or Stand!"
+        #Player card is Ace + 8 or above
+        elif getSoftTotal(playerHand) >= 19:
+            return "Stand"
+    # Dealer showing a 4
+    elif dealerCard == 4:
+        #Player card is Ace + 2 or Ace + 3
+        if getSoftTotal(playerHand) <= 14:
+            return "Hit!"
+        #Player card is Ace + 4 to Ace + 6
+        elif 15 <= getSoftTotal(playerHand) == 17:
+            return "Double!"
+        #Player card is Ace + 7
+        elif getSoftTotal(playerHand) == 18:
+            return "Double! or Stand!"
+        #Player card is Ace + 8 or above
+        elif getSoftTotal(playerHand) >= 19:
+            return "Stand"
+    # Dealer showing a 5
+    elif dealerCard == 5:
+        #Player card is Ace + 6 or below
+        if getSoftTotal(playerHand) <= 17:
+            return "Double!"
+        #Player card is Ace + 7
+        elif getSoftTotal(playerHand) == 18:
+            return "Double! or Stand!"
+        #Player card is Ace + 8 or above
+        elif getSoftTotal(playerHand) >= 19:
+            return "Stand"
+    # Dealer showing a 6
+    elif dealerCard == 6:
+        #Player card is Ace + 6 or below
+        if getSoftTotal(playerHand) <= 17:
+            return "Double!"
+        #Player card is Ace + 7
+        elif getSoftTotal(playerHand) == 18:
+            return "Double! or Stand!"
+        #Player card is Ace + 8
+        elif getSoftTotal(playerHand) == 19:
+            return "Double! or Stand!"
+        #Player card is Ace + 8
+        elif getSoftTotal(playerHand) >= 20:
+            return "Stand!"
+    # Dealer showing a 7
+    elif dealerCard == 7:
+        #Player card is Ace + 6 or below
+        if getSoftTotal(playerHand) <= 17:
+            return "Hit!"
+        #Player card is Ace + 7 or above
+        elif getSoftTotal(playerHand) >= 18:
+            return "Stand!"
+    # Dealer showing a 8
+    elif dealerCard == 8:
+        #Player card is Ace + 6 or below
+        if getSoftTotal(playerHand) <= 17:
+            return "Hit!"
+        #Player card is Ace + 7 or above
+        elif getSoftTotal(playerHand) >= 18:
+            return "Stand!"
+    # Dealer showing a 9
+    elif dealerCard == 9:
+        #Player card is Ace + 7 or below
+        if getSoftTotal(playerHand) <= 18:
+            return "Hit!"
+        #Player card is Ace + 8 or above
+        elif getSoftTotal(playerHand) >= 19:
+            return "Stand!"
+    # Dealer showing a 10
+    elif dealerCard == 10:
+        #Player card is Ace + 7 or below
+        if getSoftTotal(playerHand) <= 18:
+            return "Hit!"
+        #Player card is Ace + 8 or above
+        elif getSoftTotal(playerHand) >= 19:
+            return "Stand!"
+    # Dealer showing an Ace
+    elif dealerCard == 11:
+        #Player card is Ace + 7 or below
+        if getSoftTotal(playerHand) <= 18:
+            return "Hit!"
+        #Player card is Ace + 8 or above
+        elif getSoftTotal(playerHand) >= 19:
+            return "Stand!"
+    else:
+        return "Unknown dealer card!"
 
 # print(handIsSoft([11, 9]))
 
@@ -186,7 +289,13 @@ print(str(getHardTotal(hand)) + " " + str(dealerCard))
 print(hardHand(hand, dealerCard))
 
 
+hand = [11, random.randint(2,11)]
+dealerCard = random.randint(2,11)
 
+print()
+
+print(str(getSoftTotal(hand)) + " " + str(dealerCard))
+print(softHand(hand, dealerCard))
 
 
 
